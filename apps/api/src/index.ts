ef718a5eb6c81db7c325bc1a9cfe01f6eeebbbfe
samcxps <samsonc99@gmail.com>
2026-02-12 12:26:01 -0600

    chore: worker

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,5 +1,6 @@
 export default {
-  fetch() {
-    return new Response("Hello, world!");
+  fetch(_request: Request, env: Env) {
+    // Typed env from the alchemy.run.ts file
+    return new Response(`Hello, world! ${env.STAGE}`);
   },
-} satisfies ExportedHandler;
+} satisfies ExportedHandler<Env>;
